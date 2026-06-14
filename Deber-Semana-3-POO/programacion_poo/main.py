c6775601991a83bc6236e5246c5807fb60677a6e
Dorys-cibernética <dorystorres2010@gmail.com>
2026-06-13 23:21:38 -0500

    tercera semana poo y tradicional

---
+++ Deber-Semana-3-POO/programacion_poo/main.py
@@ -0,0 +1,28 @@
+# ==========================================
+# Programa principal
+# Gestión de Mascotas usando POO
+# ==========================================
+
+from mascota import Mascota
+
+# Crear objetos 
+mascota1 = Mascota(
+    "Max",
+    "Perro",
+    3
+)
+
+mascota2 = Mascota(
+    "Mimosa",
+    "Gata",
+    2
+)
+
+# Mostrar información y ejecutar acciones
+mascota1.mostrar_informacion()
+mascota1.hacer_sonido()
+
+print()  # Este print vacío es genial para dejar un espacio en la terminal, tal como hizo el profe
+
+mascota2.mostrar_informacion()
+mascota2.hacer_sonido()
---
+++ programacion_tradicional/tradicional.py
@@ -0,0 +1,23 @@
+# ==========================================
+# Programación Tradicional
+# Sistema básico de Mascotas
+# ==========================================
+
+def registrar_mascota():
+    nombre = input("Ingrese el nombre de la mascota: ")
+    especie = input("Ingrese la especie: ")
+    edad = input("Ingrese la edad: ")
+
+    return nombre, especie, edad
+
+
+def mostrar_mascota(nombre, especie, edad):
+    print("\n===== INFORMACIÓN DE LA MASCOTA =====")
+    print(f"Nombre  : {nombre}")
+    print(f"Especie : {especie}")
+    print(f"Edad    : {edad} años")
+
+
+# Programa principal
+nombre, especie, edad = registrar_mascota()
+mostrar_mascota(nombre, especie, edad)
